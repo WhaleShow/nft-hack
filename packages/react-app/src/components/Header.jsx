8138f2c26feeee823d5fe9684d112797e1afbb36
barrasso <barrassomark@gmail.com>
2021-03-20 00:56:38 -0400

    Edit header and title

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,10 +5,10 @@ import { PageHeader } from "antd";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/WhaleShow/nft-hack" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="👩‍🔬 Buyer pays to mint NFT example"
+        title="🐳 WhaleShow"
+        subTitle=""
         style={{ cursor: "pointer" }}
       />
     </a>
